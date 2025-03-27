599f7594d405cc250b710c3403c8bdae5c4c74e9
lilnecati <efegallagher01@gmail.com>
2025-03-27 12:33:01 +0300

    Initial Commit

---
+++ ConvertTo/ConvertToApp.swift
@@ -0,0 +1,17 @@
+//
+//  ConvertToApp.swift
+//  ConvertTo
+//
+//  Created by Necati Yıldırım on 27.03.2025.
+//
+
+import SwiftUI
+
+@main
+struct ConvertToApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ConvertToTests/ConvertToTests.swift
@@ -0,0 +1,17 @@
+//
+//  ConvertToTests.swift
+//  ConvertToTests
+//
+//  Created by Necati Yıldırım on 27.03.2025.
+//
+
+import Testing
+@testable import ConvertTo
+
+struct ConvertToTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
